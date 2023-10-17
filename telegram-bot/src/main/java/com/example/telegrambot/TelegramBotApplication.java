7a5d02eaebf5663d5bb1f89276458c520aae3d4b
Pedro Torales <pedroimanoltorales@gmail.com>
2023-10-17 16:32:35 -0300

    sprig changes

--- telegram-bot/src/main/java/com/example/telegrambot/TelegramBotApplication.java
+++ telegram-bot/src/main/java/com/example/telegrambot/TelegramBotApplication.java
@@ -2,14 +2,31 @@ package com.example.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class TelegramBotApplication {
+public class TelegramBotApplication extends SpringBootServletInitializer  {
+
+	private static ApplicationContext applicationContext;
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TelegramBotApplication.class);
+	}
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(TelegramBotApplication.class, args);
+		applicationContext = SpringApplication.run(TelegramBotApplication.class, args);
 
+	}
+
+	private static void checkBeansPresence(String... beans) {
+		for (String beanName : beans) {
+			System.out.println("Is " + beanName + " in ApplicationContext: " +
+					applicationContext.containsBean(beanName));
+		}
 	}
 
 }
